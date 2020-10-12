e25603c9aa51be1e8a6026ba1d921d06b6bc2dce
ekridikriduck <mailnadeem1508@gmail.com>
2020-10-12 17:43:38 +0530

    added basename for git deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import theme from "./theme";
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-        <Router>
+        <Router basename={window.location.pathname || ''}>
             <Routes/>
         </Router>
         </ThemeProvider>
