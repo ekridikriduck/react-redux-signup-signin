188bb8be26018dc4765c61ae25a1f844a4fe61bf
ekridikriduck <mailnadeem1508@gmail.com>
2020-10-12 17:24:32 +0530

    Updates

---
+++ precache-manifest.06072a1d92e4417eaed806292b62fe1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e9e50ac93206eb64e1056f730e2da3",
+    "url": "/react-redux-signup-signin/index.html"
+  },
+  {
+    "revision": "28c1abf6b33969a5d194",
+    "url": "/react-redux-signup-signin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e767266f8876c172009b",
+    "url": "/react-redux-signup-signin/static/js/2.7e08019d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-signup-signin/static/js/2.7e08019d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c1abf6b33969a5d194",
+    "url": "/react-redux-signup-signin/static/js/main.52a5818c.chunk.js"
+  },
+  {
+    "revision": "bf5b9fa8c4bb44f943f7",
+    "url": "/react-redux-signup-signin/static/js/runtime-main.3fcdaf7c.js"
+  }
+]);
